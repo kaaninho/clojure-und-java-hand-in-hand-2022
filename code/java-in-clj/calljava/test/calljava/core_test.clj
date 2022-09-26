0a2b8afe33808c9ddcbdecd74e3db49d59453219
Kaan Sahin <kaan.sahin@active-group.de>
2022-09-26 18:54:12 +0200

    Techcamp version

--- code/java-in-clj/calljava/test/calljava/core_test.clj
+++ code/java-in-clj/calljava/test/calljava/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [calljava.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest long-names-test
+  (is (= ["my-long-name"]
+         (long-names #{"kaan" "my-long-name"}))))
