00f91180c213c232c95a5f5c190b2620664cec55
Kaan Sahin <kaan.sahin@active-group.de>
2023-06-12 10:25:17 +0200

    Create cheat sheet

--- code/java-in-clj/calljava/src/calljava/core.clj
+++ code/java-in-clj/calljava/src/calljava/core.clj
@@ -58,41 +58,83 @@
 
 
 
-;; vollqualifiziert
 
-;; mit `import`
-#_(Date.)
 
-;;; Dann eigene Java Libraries
 
-(def thePoint (Point. 1 2))
 
-(.-x thePoint)
 
-(.distanceToPoint thePoint (Point. 0 0))
 
-(defn make-point [x y]
-  (Point. x y))
 
-(make-point 3 4)
 
-(defn distance-to-origin [point]
-  (.distanceToPoint point (Point. 0 0)))
 
-(distance-to-origin (Point. 3 4))
 
+
+
+
+
+
+
+
+;; (ns calljava.core
+;;   (:import java.util.Date
+;;            Point
+;;            Distance))
+
+
+;; ;;; 1. REPL und Editor-Magic
+
+;; (+ 1 2)
+
+;; (map inc [1 2 3 4])
+
+;; (filter even? [1 2 3 4 5])
+
+;; ;;; 2. Java-interne Libraries
+
+;; (def the-date (java.util.Date.))
+
+;; (.toString (java.util.Date.))
+
+;; ;; dann importieren
+
+;; (Date.)
+
+;; (.toString the-date)
+
+;; (str the-date)
+
+;; ;;; 3. eigene Libraries
+
+;; (def p (Point. 3 4))
+
+;; (str p)
+
+;; (.-y p)
+
+;; (defn make-point [x y]
+;;   (Point. x y))
+
+;; (make-point 3 4)
+
+
+;; (defn distance-to-origin [point]
+;;   (.distanceToPoint point (Point. 0 0)))
+
+;; (distance-to-origin p)
+
+;;; Neuer Record
+
+;; (defrecord Point3D [x y z])
 
 ;; Interfaces implementieren
-
-(defrecord Point3D [x y z]
-  Object
-  (toString [_] (str "X-Koord: " x ", Y-Koord: " y "Z-Koord: " z)))
-
-(str (->Point3D 1 2 3))
-
+;; (erst toString)
 
 ;; (defrecord Point3D [x y z]
+;;   Object
+;;   (toString [_] (str "X-Koord: " x ", Y-Koord: " y ", Z-Koord: " z))
 ;;   Distance
-;;   (distanceToOrigin [this] 5.0))
-
-;; (.distanceToOrigin (->Point3D 1 2 3))
+;;   (distanceToOrigin [_] (Math/sqrt (+ (* x x) (* y y) (* z z)))))
+;;
+;; (str (->Point3D 1 2 3))
+;;
+;; (.distanceToOrigin (->Point3D 0 3 4))
